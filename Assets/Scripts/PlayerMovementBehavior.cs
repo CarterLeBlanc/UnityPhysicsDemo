ed071bf3f451cb3feb7f13438151f91c9407eb1f
Carter LeBlanc <cartermleblanc@gmail.com>
2020-09-24 16:58:42 -0500

    Created scene with ragdoll animations and player movement.

---
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    private CharacterController _controller = null;
+    private Animator _animator = null;
+
+    public float speed = 1.0f;
+    public float turnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set the movement to be equal to the input
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        //Move the player
+        _controller.SimpleMove(transform.forward * movement.z * speed);
+        transform.Rotate(transform.up, movement.x * turnRate);
+
+        //Animate based on movement
+        _animator.SetFloat("Speed", movement.z * speed);
+
+        Debug.DrawRay(transform.position, transform.forward);
+
+        //Fire when the space key is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                RagdollBehavior ragdoll = hit.transform.GetComponent<RagdollBehavior>();
+                if (ragdoll != null)
+                {
+                    ragdoll.ragdollEnabled = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RagdollBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RagdollBehavior : MonoBehaviour
+{
+    private Animator _animator = null;
+    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+    public bool ragdollEnabled
+    {
+        get { return !_animator.enabled; }
+
+        set
+        {
+            _animator.enabled = !value;
+
+            foreach (Rigidbody rb in rigidbodies)
+            {
+                rb.isKinematic = !value;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        ragdollEnabled = false;
+    }
+}
